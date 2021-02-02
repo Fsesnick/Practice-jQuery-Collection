341c6640490c11eed04ccc7010998ef6844eb6d6
Fsesnick <fernandasesnick@gmail.com>
2021-02-02 17:40:03 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// 1. Adicione uma tag "featured" ao segundo item da lista:
+   // Anexe um novo elemento span contendo a palavra "Featured" ao item da lista
+   // Adicione uma classe de "featured" ao novo elemento span.
+
+// 2. Adicione uma classe de "new" ao último item da lista. Você pode ir até o último item ou encontrar um método jQuery que o ajude a selecionar o último item da lista.
+
+// 3. Adicione um atributo a cada link para que todas as tags âncora sejam abertas em uma nova guia.
+
+// 4. Selecione e imprima o índice e o texto de cada tag âncora para o console
+
+// 5. Refatore a pergunta nº 4 usando a palavra-chave 'this'. 
